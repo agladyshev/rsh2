b01754da62b538383feafbebf426336207c2771e
Sallie Tiffiny <stiffiny@su-i1.msk.21-school.ru>
2021-02-07 16:56:43 +0300

    do not trim spaces from file

--- ex00/srcs/parse_dict.c
+++ ex00/srcs/parse_dict.c
@@ -6,7 +6,7 @@
 /*   By: stiffiny <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/06 16:13:45 by stiffiny          #+#    #+#             */
-/*   Updated: 2021/02/06 17:53:01 by stiffiny         ###   ########.fr       */
+/*   Updated: 2021/02/07 16:23:16 by stiffiny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,20 +26,17 @@ int	get_dict_len(char *filename)
 	int		file;
 
 	file = open(filename, 0);
-	
 	if (file < 0)
 		return (0);
-	//error handling
 	bytes = -1;
 	chars = 0;
 	while(bytes)
 	{
 		bytes = read(file, buf, 1);
-		if (bytes && buf[0] != ' ')
+		if (bytes)
 			chars++;
 	}
 	close(file);
-	printf("Chars: %d\n", chars);
 	return (chars);
 }
 
@@ -53,32 +50,21 @@ char	*parse_dict_file(char *filename)
 	int	i;
 
 	dict_len = get_dict_len(filename);
-	if (!dict_len)
-		return (0);
 	str = malloc(sizeof(char) * (dict_len + 1));
-	if (!str)
-		return (str);
-	// couldn't allocate memory error	
-	
 	file = open(filename, 0);
-	if (!file)
+	if (!dict_len || !str || file < 0)
 		return (0);
-	//error handling
-	
 	bytes = -1;
 	i = 0;
 	while (bytes)
 	{
 		bytes = read(file, buf, 1);
-		//printf("Bytes: %d, i: %d, buf: %s\n", bytes, i, buf);
-		if (buf[0] != ' ' && bytes)
+		if (bytes)
 		{
 			str[i] = buf[0];
 			i++;
 		}
 	}	
-	printf("Is i = len ? %d %d\n", i,  dict_len);
 	str[i] = '\0';
-
 	return (str);
 }
